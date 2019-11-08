7c6012e517e38a1576a01ab542c55f9f34ea3352
darivs <dtack192@gmail.com>
2019-11-08 16:10:53 +0100

    Remove example code

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,21 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/logo.png"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
         />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-
     <v-content>
-      <router-view />
+      <v-container fill-height>
+        <v-row justify="center">
+          <v-col cols="12" class="py-2">
+            <v-row justify="center" align="center">
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,24 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
-import de from 'vuetify/es5/locale/de';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import de from "vuetify/es5/locale/de";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    lang: {
-      locales: { de },
-      current: 'de',
-    },
+  lang: {
+    locales: { de },
+    current: "de"
+  },
+  icons: {
+    iconfont: "mdiSvg"
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: "#556B93",
+        secondary: "#DAEBF3",
+        accent: "#B7C533"
+      }
+    }
+  }
 });
