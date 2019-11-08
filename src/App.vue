d89aca48d81b0d4c30b00b03361f0b3adaa21cce
darivs <dtack192@gmail.com>
2019-11-08 15:55:45 +0100

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -38,23 +34,16 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
+  name: "App",
   data: () => ({
     //
-  }),
+  })
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./plugins/router";
 import vuetify from "./plugins/vuetify";
 import firebase from "./plugins/firebase";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   vuetify,
   firebase,
   render: h => h(App)
