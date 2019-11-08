39c35bd8c92cc86891ea8f848e9dd7e31297987a
darivs <dtack192@gmail.com>
2019-11-08 16:04:48 +0100

    Remove vue-router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./plugins/router";
 import vuetify from "./plugins/vuetify";
 import firebase from "./plugins/firebase";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   vuetify,
   firebase,
   render: h => h(App)
