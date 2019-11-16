476b01ef9bf9919d7588d9ef3cdec38b6a7c30e1
darivs <dtack192@gmail.com>
2019-11-16 22:55:42 +0100

    Create centered column component

---
+++ src/components/VColCentered.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-col :cols="cols">
+    <v-row justify="center" align="center" class="primary--text subtitle-1">
+      <slot />
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "VColCentered",
+  props: {
+    cols: {
+      type: String,
+      default: () => "12",
+    },
+  },
+}
+</script>
+
+<style scoped>
+.col,
+.row {
+  padding: 0 !important;
+  margin: 0 !important;
+}
+</style>
